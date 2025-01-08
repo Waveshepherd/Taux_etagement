b552e36e2bac5603a6d2c70f2984cef9b344956d
richaben <brichard.rb@gmail.com>
2025-01-08 13:45:50 +0100

    modif scripts

--- scripts/00_chargement_packages.R
+++ scripts/00_chargement_packages.R
@@ -15,21 +15,23 @@
 ### Restaure les packages dans le lockfile :
 
 library(tidyverse)
-library(tidyr)
+#library(tidyr) <- fait partie de tidyverse
 library(plyr)
-library(tibble)
+#library(tibble) <- fait partie de tidyverse
 library(shiny)
 library(rJava)
 library(readxl)
-library(readr)
+#library(readr) <- fait partie de tidyverse
 library(knitr)
-library(ggplot2)
+#library(ggplot2) <- fait partie de tidyverse
 library(ggrepel)
 library(patchwork)
 library(plotly)
-library(dplyr)
+#library(dplyr) <- fait partie de tidyverse
 library(DT)
 library(reactable)
 library(scales)
 library(rlang)
 library(rvest)
+
+library(r4geobs)
